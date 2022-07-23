95660838c2c9d126025a980af7ed697c40fe9a65
anool.m@gmail.com <anool.m@gmail.com>
2022-07-23 11:54:00 +0530

    updated matrix 16x7 pixels with scrolling text restored

--- DrillPressCafeScroll/DrillPressCafeScroll.ino
+++ DrillPressCafeScroll/DrillPressCafeScroll.ino
@@ -1,22 +1,58 @@
+/*
+  Maker's Asylum
+  16x7 NeoPixel matrix Beer Bottle Display for Drill Press Cafe
+  Modified 23 Jul 2022
+  by Anool Mahidharia
+  This example code is in the public domain.
+*/
+
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 #ifndef PSTR
- #define PSTR 
+#define PSTR 
 #endif
 
-#define PIN 6
-const int width = 7;
-const int height = 7;
+#define PIN 6 // data pin, usual default is D6
+#define mw 16 // width of the matrix in no of pixels
+#define mh 7 // height of the matrix in no of pixels
 
-Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(width, height, PIN, NEO_MATRIX_ZIGZAG, NEO_GRB+NEO_KHZ400);
 
-const uint16_t colors[] = {matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(mw, mh, PIN,
+  NEO_MATRIX_TOP    + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255),
+  matrix.Color(255, 255, 0), matrix.Color(0, 255, 255), matrix.Color(255, 0, 255),
+  matrix.Color(255, 255, 255), matrix.Color(100, 255, 100), matrix.Color(200, 100, 255)};
 
 void setup() {
   matrix.begin();
   matrix.setTextWrap(false);
-  matrix.setBrightness(70);
+  matrix.setBrightness(90);
   matrix.setTextColor(colors[0]);
 }
 
@@ -26,12 +62,13 @@ int pass = 0;
 void loop() {
   matrix.fillScreen(0);
   matrix.setCursor(x,0);
-  matrix.print(F("Drill Press Cafe"));
-  if(--x < -50) {
+  matrix.print(F("Drill Press Cafe!"));
+//I'm not sure what the below number "100" does, but the original "50" didn't work - text would not scroll completely. So I progressively increased it until it worked.
+  if(--x < -100) {
     x = matrix.width();
-    if(++pass >= 3) pass = 0;
+    if(++pass >= 9) pass = 0;
     matrix.setTextColor(colors[pass]);
   }
   matrix.show();
-  delay(70);
+  delay(150);
 }
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # BeerBottleDisplay
-the **Drill Press Cafe** beer bottle display code
+this is the **Drill Press Cafe** beer bottle display code
 
+***UPDATED Matrix***
+- Old 16x5 matrix updated to 16x7 matrix on 23 Jul 2022.
+- The matrix can now run scrolling text using a 7x5 font.
+- Uses code from the /DrillPressCafeScroll folder
+- Hareware is an Arduino Nano with "old bootloader"
+- Data Pin for Pixels is D6
+- +5V power is derived from a 5V/50W SMPS
+- LED's are arranged in horizontal rows, zig-zag fashion, with first LED at top_left of matrix
+
+***Old Version***
 - Initial code was for a scrolling display with a 16 pixel Wide x 7 pixel Tall matrix (16x7).
 - Existing matrix is 16x6 so the scrolling matrix code no longer works.
 - Use the "Matrix_Designer" Spreadsheet to design your characters - RED cells are the LEDs which will be turned on.
